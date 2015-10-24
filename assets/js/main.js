0d9be8fa07e78b5793a29681d3992c9cd0a9e7f3
Damian Plewa <damianp96@gmail.com>
2015-10-24 19:32:21 +0200

    sporo zmian, na lepsze, dodałem menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,51 +14,44 @@ for(var x = 0; x < cardsLen; x++){
 	var card = cards[0];
 	var cardTitle = cards[0].attributes.getNamedItem('cardTitle').value;
 	var cardCover = cards[0].attributes.getNamedItem('cardCover').value;
-	var cardAction = cards[0].attributes.getNamedItem('cardAction').value;
 	var cardActionHref = cards[0].attributes.getNamedItem('cardActionHref').value;
-	console.log(cardTitle, cardCover, cardAction, cardActionHref);
 	
 	var divBody = document.createElement("div");
-	divBody.classList.add("mdl-card", "mdl-shadow--2dp");
+	divBody.classList.add("mdl-card");
+	
+	var aCover = document.createElement('a');
+	aCover.href = cardActionHref;
 	
 	var divTitleParent = document.createElement("div");
 	divTitleParent.classList.add('mdl-card__title', 'mdl-card--expand');
 	if(cardCover == "" || typeof(cardCover) == 'undefined')
-		divTitleParent.style.background = "url('https://fanart.tv/fanart/music/c7423e0c-ab3e-4ab4-be10-cdff5a9d3062/cdart/fire-of-unknown-origin-52dc4a58c114f.png') top no-repeat #FFF";
-	else
+		divTitleParent.style.background = "url(http://192.168.2.107:8080/assets/img/music_album.png) 50% 50% / 75% no-repeat rgb(255, 255, 255)";
+	else{
 		divTitleParent.style.background = "url('"+cardCover+"') top no-repeat #FFF";
-	divTitleParent.style.backgroundSize = "100%";
+		divTitleParent.style.backgroundSize = "100%";
+	}
+	aCover.appendChild(divTitleParent);
 	
 	var h2Title = document.createElement('h2');
-	h2Title.innerHTML = cardTitle;
+	var htmlCODE = '\
+	<button id="id-card-'+x+'" class="mdl-button mdl-js-button mdl-button--icon">\
+	<i class="material-icons">more_vert</i></button>\
+	<ul class="mdl-menu mdl-menu--top-right mdl-js-menu mdl-js-ripple-effect" for="id-card-'+x+'">\
+		<li class="mdl-menu__item">Dodaj do koszyka</li>\
+		<li class="mdl-menu__item" onclick="window.location = \''+cardActionHref+'\'">Pokaż więcej informacji</li>\
+		<li class="mdl-menu__item" onclick="window.location = \'https://www.google.pl/webhp#q='+cardTitle+'\'">Szukaj w Google</li>\
+		<li class="mdl-menu__item" onclick="window.location = \'https://pl.wikipedia.org/wiki/'+cardTitle+'\'">Szukaj na Wikipedii</li>\
+	</ul>';
+	h2Title.innerHTML = cardTitle + htmlCODE;
 	h2Title.classList.add('mdl-card__title-text');
-	//divTitleParent.appendChild(h2Title);
 	
 	var divActions = document.createElement('div');
 	divActions.classList.add('mdl-card__actions');
 	
-	var aLink = document.createElement("a");
-	aLink.classList.add('mdl-button', 'mdl-button--colored', 'mdl-js-button', 'mdl-js-ripple-effect');
-	aLink.innerText = cardAction;
-	aLink.href = cardActionHref;
 	divActions.appendChild(h2Title);
-	divActions.appendChild(aLink);
 	
-	var divCardMenu = document.createElement("div");
-	divCardMenu.classList.add('mdl-card__menu');
-	
-	var buttonMenu = document.createElement('button');
-	buttonMenu.classList.add('mdl-button', 'mdl-button--icon', 'mdl-js-button', 'mdl-js-ripple-effect');
-	
-	var iIcon = document.createElement("i");
-	iIcon.classList.add("material-icons");
-	iIcon.innerText = "share";
-	buttonMenu.appendChild(iIcon);
-	divCardMenu.appendChild(buttonMenu);
-	
-	divBody.appendChild(divTitleParent);
+	divBody.appendChild(aCover);
 	divBody.appendChild(divActions);
-	divBody.appendChild(divCardMenu);
 	
 	card.parentNode.insertBefore(divBody, card);
 	card.parentNode.removeChild(card);
