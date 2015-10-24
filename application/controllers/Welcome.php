9c2db0005d42eba7b55f1b1ff7046e948c48834b
Damian Plewa <damianp96@gmail.com>
2015-10-24 14:20:15 +0200

    Zmieniłem sobie lay od welcome

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,6 +20,8 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('layout/header');
+		$this->load->view('layout/testmid');
+		$this->load->view('layout/footer');
 	}
 }
