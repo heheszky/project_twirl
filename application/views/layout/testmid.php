92e7cbfad71e1942023224bb8013861d55b3a0da
Damian Plewa <damianp96@gmail.com>
2015-10-24 19:30:43 +0200

    Dodałem kilka sekcji do testowego wyglądu

--- application/views/layout/testmid.php
+++ application/views/layout/testmid.php
@@ -1,14 +1,41 @@
 <div id='Music' class='homeCategory'>
   <h4>Muzyka <button class='mdl-button mdl-js-button'>więcej</button></h4>
   <div class='itemSection'>
-    <card cardTitle='Eminem' cardCover='http://ecx.images-amazon.com/images/I/71HH7D7Z66L._SL1500_.jpg' cardAction='Zamów' cardActionHref='#'></card>
-    <card cardTitle='Trelemorele' cardCover='' cardAction='Zamów' cardActionHref='#'></card>
-    <card cardTitle='Eminem' cardCover='http://ecx.images-amazon.com/images/I/71HH7D7Z66L._SL1500_.jpg' cardAction='Zamów' cardActionHref='#'></card>
-    <card cardTitle='Trelemorele' cardCover='' cardAction='Zamów' cardActionHref='#'></card>
-    <card cardTitle='Eminem' cardCover='http://ecx.images-amazon.com/images/I/71HH7D7Z66L._SL1500_.jpg' cardAction='Zamów' cardActionHref='#'></card>
-    <card cardTitle='Trelemorele' cardCover='' cardAction='Zamów' cardActionHref='#'></card>
-    <card cardTitle='Eminem' cardCover='http://ecx.images-amazon.com/images/I/71HH7D7Z66L._SL1500_.jpg' cardAction='Zamów' cardActionHref='#'></card>
-    <card cardTitle='Trelemorele' cardCover='' cardAction='Zamów' cardActionHref='#'></card>
-    
+    <card cardTitle='Eminem' cardCover='http://ecx.images-amazon.com/images/I/71HH7D7Z66L._SL1500_.jpg'  cardActionHref='#'></card>
+    <card cardTitle='Trelemorele' cardCover=''  cardActionHref='#'></card>
+    <card cardTitle='Eminem' cardCover='http://ecx.images-amazon.com/images/I/71HH7D7Z66L._SL1500_.jpg'  cardActionHref='#'></card>
+    <card cardTitle='Trelemorele' cardCover=''  cardActionHref='#'></card>
+    <card cardTitle='Eminem' cardCover='http://ecx.images-amazon.com/images/I/71HH7D7Z66L._SL1500_.jpg'  cardActionHref='#'></card>
+    <card cardTitle='Trelemorele' cardCover=''  cardActionHref='#'></card>
+    <card cardTitle='Eminem' cardCover='http://ecx.images-amazon.com/images/I/71HH7D7Z66L._SL1500_.jpg'  cardActionHref='#'></card>
+    <card cardTitle='Trelemorele' cardCover=''  cardActionHref='#'></card>
+  </div>
+</div>
+
+<div id='Film' class='homeCategory'>
+  <h4>Filmy <button class='mdl-button mdl-js-button'>więcej</button></h4>
+  <div class='itemSection'>
+    <card cardTitle='Eminem' cardCover='http://ecx.images-amazon.com/images/I/71HH7D7Z66L._SL1500_.jpg'  cardActionHref='#'></card>
+    <card cardTitle='Trelemorele' cardCover=''  cardActionHref='#'></card>
+    <card cardTitle='Eminem' cardCover='http://ecx.images-amazon.com/images/I/71HH7D7Z66L._SL1500_.jpg'  cardActionHref='#'></card>
+    <card cardTitle='Trelemorele' cardCover=''  cardActionHref='#'></card>
+    <card cardTitle='Eminem' cardCover='http://ecx.images-amazon.com/images/I/71HH7D7Z66L._SL1500_.jpg'  cardActionHref='#'></card>
+    <card cardTitle='Trelemorele' cardCover=''  cardActionHref='#'></card>
+    <card cardTitle='Eminem' cardCover='http://ecx.images-amazon.com/images/I/71HH7D7Z66L._SL1500_.jpg'  cardActionHref='#'></card>
+    <card cardTitle='Trelemorele' cardCover=''  cardActionHref='#'></card>
+  </div>
+</div>
+
+<div id='Books' class='homeCategory'>
+  <h4>Książki <button class='mdl-button mdl-js-button'>więcej</button></h4>
+  <div class='itemSection'>
+    <card cardTitle='Eminem' cardCover='http://ecx.images-amazon.com/images/I/71HH7D7Z66L._SL1500_.jpg'  cardActionHref='#'></card>
+    <card cardTitle='Trelemorele' cardCover=''  cardActionHref='#'></card>
+    <card cardTitle='Eminem' cardCover='http://ecx.images-amazon.com/images/I/71HH7D7Z66L._SL1500_.jpg'  cardActionHref='#'></card>
+    <card cardTitle='Trelemorele' cardCover=''  cardActionHref='#'></card>
+    <card cardTitle='Eminem' cardCover='http://ecx.images-amazon.com/images/I/71HH7D7Z66L._SL1500_.jpg'  cardActionHref='#'></card>
+    <card cardTitle='Trelemorele' cardCover=''  cardActionHref='#'></card>
+    <card cardTitle='Eminem' cardCover='http://ecx.images-amazon.com/images/I/71HH7D7Z66L._SL1500_.jpg'  cardActionHref='#'></card>
+    <card cardTitle='Trelemorele' cardCover=''  cardActionHref='#'></card>
   </div>
 </div>
