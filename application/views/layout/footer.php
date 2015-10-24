d43539b53624f8c3ebf30354e6eaa5686b4c9765
Damian Plewa <damianp96@gmail.com>
2015-10-24 14:20:36 +0200

    Zrobiłem template headera, footera i przykladowy content

---
+++ application/views/layout/footer.php
@@ -0,0 +1,17 @@
+				</main>
+				<div class="mdl-layout__obfuscator"></div>
+				<footer class="mdl-mini-footer">
+					<div class="mdl-mini-footer__left-section">
+						<div class="mdl-logo">Heheszki</div>
+						<ul class="mdl-mini-footer__link-list">
+						<li><a href="#">Pomoczy</a></li>
+						<li><a href="#">Dokumentacja</a></li>
+						</ul>
+					</div>
+				</footer>
+			</div>
+		</div>
+	</body>
+	<script src="/assets/js/material.min.js"></script>
+	<script src='/assets/js/main.js'></script>
+</html>
---
+++ application/views/layout/header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html class="mdl-js">
+	<head>
+		<title>Projekt</title>
+		
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta charset='utf-8'>
+		
+		<link href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en" rel="stylesheet" type="text/css">
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+		<link rel="stylesheet" href="/assets/css/material.min.css">
+		<link rel='stylesheet' href='/assets/css/header.css'>
+		<link rel='stylesheet' href='/assets/css/main.css'>
+		<link rel='stylesheet' href='/assets/css/cards.css'>
+	</head>
+	<body>
+		<div class='mdl-layout mdl-js-layout'>
+			<div class='mdl-layout mdl-js-layout is-upgraded' data-upgraded=",MaterialLayout" >
+				<header class='mdl-layout__header mdl-layout__header--transparent' style='background: #263238'>
+					<div class="mdl-layout-icon"></div>
+					<div class="mdl-layout__header-row">
+						<span class="mdl-layout-title">Wypożyczalnia</span>
+						<div class="mdl-layout-spacer"></div>
+						<nav class="mdl-navigation">
+							<a class="mdl-navigation__link" href="#">Wszystkie</a>
+							<a class="mdl-navigation__link" href="#">Książki</a>
+							<a class="mdl-navigation__link" href="#">Filmy</a>
+							<a class="mdl-navigation__link" href="#">Muzyka</a>
+						</nav>
+					</div>
+				</header>
+				<div class="mdl-layout__drawer" id='drawer'>
+					<span class="mdl-layout-title">Kategorie</span>
+					<nav class="mdl-navigation">
+					<a class="mdl-navigation__link" href="">Wszystkie</a>
+					<a class="mdl-navigation__link" href="">Książki</a>
+					<a class="mdl-navigation__link" href="">Filmy</a>
+					<a class="mdl-navigation__link" href="">Muzyka</a>
+					</nav>
+				</div>
+				<div class="mdl-layout__drawer-button"><i class="material-icons">menu</i></div>
+				<div class='small-screen-header'>Wypożyczalnia</div>
+				<main class='mdl-layout__content mdl-color-text--grey-600'>
