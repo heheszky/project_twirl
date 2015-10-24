70f64153b7d8023ae340f59a03e266d750865088
Damian Plewa <damianp96@gmail.com>
2015-10-24 15:24:11 +0200

    zrobiłem "tag" karty, żeby nie było dużo kodu

--- application/views/layout/testmid.php
+++ application/views/layout/testmid.php
@@ -1,18 +1,14 @@
-<div id='Music'>
+<div id='Music' class='homeCategory'>
   <h4>Muzyka <button class='mdl-button mdl-js-button'>więcej</button></h4>
-  <div class="mdl-card mdl-shadow--2dp">
-    <div class="mdl-card__title mdl-card--expand" style="background:url('http://ecx.images-amazon.com/images/I/71HH7D7Z66L._SL1500_.jpg') top no-repeat #46B6AC;background-size:100%;">
-      <h2 class="mdl-card__title-text">Update</h2>
-    </div>
-    <div class="mdl-card__actions mdl-card--border">
-      <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
-        Zamów
-      </a>
-    </div>
-    <div class="mdl-card__menu">
-      <button class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
-        <i class="material-icons">share</i>
-      </button>
-    </div>
+  <div class='itemSection'>
+    <card cardTitle='Eminem' cardCover='http://ecx.images-amazon.com/images/I/71HH7D7Z66L._SL1500_.jpg' cardAction='Zamów' cardActionHref='#'></card>
+    <card cardTitle='Trelemorele' cardCover='' cardAction='Zamów' cardActionHref='#'></card>
+    <card cardTitle='Eminem' cardCover='http://ecx.images-amazon.com/images/I/71HH7D7Z66L._SL1500_.jpg' cardAction='Zamów' cardActionHref='#'></card>
+    <card cardTitle='Trelemorele' cardCover='' cardAction='Zamów' cardActionHref='#'></card>
+    <card cardTitle='Eminem' cardCover='http://ecx.images-amazon.com/images/I/71HH7D7Z66L._SL1500_.jpg' cardAction='Zamów' cardActionHref='#'></card>
+    <card cardTitle='Trelemorele' cardCover='' cardAction='Zamów' cardActionHref='#'></card>
+    <card cardTitle='Eminem' cardCover='http://ecx.images-amazon.com/images/I/71HH7D7Z66L._SL1500_.jpg' cardAction='Zamów' cardActionHref='#'></card>
+    <card cardTitle='Trelemorele' cardCover='' cardAction='Zamów' cardActionHref='#'></card>
+    
   </div>
 </div>
--- assets/js/main.js
+++ assets/js/main.js
@@ -11,13 +11,55 @@ var cards = document.getElementsByTagName("card");
 var cardsLen = cards.length;
 
 for(var x = 0; x < cardsLen; x++){
-	var card = cards[x];
-	var cardTitle = cards[x].attributes.getNamedItem('cardTitle').value;
-	var cardCover = cards[x].attributes.getNamedItem('cardCover').value;
-	var cardAction = cards[x].attributes.getNamedItem('cardAction').value;
-	var cardActionHref = cards[x].attributes.getNamedItem('cardActionHref').value;
+	var card = cards[0];
+	var cardTitle = cards[0].attributes.getNamedItem('cardTitle').value;
+	var cardCover = cards[0].attributes.getNamedItem('cardCover').value;
+	var cardAction = cards[0].attributes.getNamedItem('cardAction').value;
+	var cardActionHref = cards[0].attributes.getNamedItem('cardActionHref').value;
 	console.log(cardTitle, cardCover, cardAction, cardActionHref);
 	
 	var divBody = document.createElement("div");
+	divBody.classList.add("mdl-card", "mdl-shadow--2dp");
 	
+	var divTitleParent = document.createElement("div");
+	divTitleParent.classList.add('mdl-card__title', 'mdl-card--expand');
+	if(cardCover == "" || typeof(cardCover) == 'undefined')
+		divTitleParent.style.background = "url('https://fanart.tv/fanart/music/c7423e0c-ab3e-4ab4-be10-cdff5a9d3062/cdart/fire-of-unknown-origin-52dc4a58c114f.png') top no-repeat #FFF";
+	else
+		divTitleParent.style.background = "url('"+cardCover+"') top no-repeat #FFF";
+	divTitleParent.style.backgroundSize = "100%";
+	
+	var h2Title = document.createElement('h2');
+	h2Title.innerHTML = cardTitle;
+	h2Title.classList.add('mdl-card__title-text');
+	//divTitleParent.appendChild(h2Title);
+	
+	var divActions = document.createElement('div');
+	divActions.classList.add('mdl-card__actions');
+	
+	var aLink = document.createElement("a");
+	aLink.classList.add('mdl-button', 'mdl-button--colored', 'mdl-js-button', 'mdl-js-ripple-effect');
+	aLink.innerText = cardAction;
+	aLink.href = cardActionHref;
+	divActions.appendChild(h2Title);
+	divActions.appendChild(aLink);
+	
+	var divCardMenu = document.createElement("div");
+	divCardMenu.classList.add('mdl-card__menu');
+	
+	var buttonMenu = document.createElement('button');
+	buttonMenu.classList.add('mdl-button', 'mdl-button--icon', 'mdl-js-button', 'mdl-js-ripple-effect');
+	
+	var iIcon = document.createElement("i");
+	iIcon.classList.add("material-icons");
+	iIcon.innerText = "share";
+	buttonMenu.appendChild(iIcon);
+	divCardMenu.appendChild(buttonMenu);
+	
+	divBody.appendChild(divTitleParent);
+	divBody.appendChild(divActions);
+	divBody.appendChild(divCardMenu);
+	
+	card.parentNode.insertBefore(divBody, card);
+	card.parentNode.removeChild(card);
 }
