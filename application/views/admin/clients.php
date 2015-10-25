2161c8af6cbb405035b53c7a7dbf1e84f1ca4442
R4ndom <damianp96@gmail.com>
2015-10-25 16:06:25 +0100

    ostylowany plik clients.php

--- application/views/admin/clients.php
+++ application/views/admin/clients.php
@@ -1,9 +1,32 @@
-<?php foreach($clients as $client): ?>
-	<h3><?= $client->imie_klienta." ". $client->nazwisko_klienta ?></h3>
-	<p>ID: <?= $client->ID_klienta ?></p>
-	<p>Email: <?= $client->email ?></p>
-	<p>Miejscowość: <?= $client->miejscowosc ?></p>
-	<p>Adres: <?= $client->ulica." ".$client->nr_domu."/".$client->nr_lokalu ?></p>
-	<p>Data urodzenia: <?= $client->data_urodzenia ?></p>
-	<p>PESEL: <?= $client->pesel ?></p>
-<?php endforeach; ?>
+<div class='midcontent centered'>
+  <table class="mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp table-centered">
+    <thead>
+      <tr>
+      <th>ID</th>
+        <th class="mdl-data-table__cell--non-numeric">Imię</th>
+        <th>Nazwisko</th>
+        <th>Email</th>
+      <th>Miejscowość</th>
+      <th>Adres</th>
+      <th>Data urodzenia</th>
+      <th>PESEL</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach($clients as $client): ?>
+      <tr>
+        <td><?= $client->ID_klienta ?></td>
+        <td><?= $client->imie_klienta ?></td>
+        <td><?= $client->nazwisko_klienta ?></td>
+        <td><?= $client->email ?></td>
+        <td><?= $client->miejscowosc ?></td>
+        <td><?= $client->ulica." ".$client->nr_domu."/".$client->nr_lokalu ?></td>
+        <td><?= $client->data_urodzenia ?></td>
+        <td><?= $client->pesel ?></td>
+      </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
+
+
+</div>
