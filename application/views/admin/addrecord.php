86c6d20f02faefb712320db0305ddea4d51f452f
R4ndom <damianp96@gmail.com>
2015-10-25 19:35:29 +0100

    dodawnie rekordów zmienione na takie ładne =D

--- application/views/admin/addrecord.php
+++ application/views/admin/addrecord.php
@@ -1,88 +1,141 @@
-<!------------------DODAWANIE AUTORA------------------------------->
-<h2>Autor</h2>
-<form action="/admin/addrecord" method="POST">
-	<input type="hidden" name="action" value="autor">
-	<input type="text" name="imie_autora" placeholder="Imie"/>
-	<?= form_error('imie_autora'); ?>
-	<input type="text" name="nazwisko_autora" placeholder="Nazwisko"/>
-	<?= form_error('nazwisko_autora'); ?>
-	<input type="text" name="pseudonim_autora" placeholder="Pseudonim"/>
-	<?= form_error('pseudonim_autora'); ?>
-	<input type="date" name="data_urodzenia_autora"/>
-	<?= form_error('data_urodzenia_autora'); ?>
-	<input type="date" name="data_smierci_autora"/>
-	<?= form_error('data_smierci_autora'); ?>
-	<select name="kraj_autora">
-	<?php foreach($kraje as $kraj): ?>
-		<option value="<?= $kraj->ID_kraju ?>">
-			<?= $kraj->nazwa_kraju ?>
-		</option>
-	<?php endforeach; ?>
-	</select>
-	<?= form_error('kraj_autora'); ?>
-	<select name="typ_autora">
-		<option disabled selected>---</option>
-		<option value=1>Pisarz</option>
-		<option value=2>Reżyser</option>
-		<option value=3>Piosenkarz</option>
-	</select>
-	<?= form_error('typ_autora'); ?>
-	<input type="submit" value="Dodaj">
-</form>
-
 <!------------------DODAWANIE KSIĄŻKI------------------------------->
-<h2>Książka</h2>
-<form action="/admin/addrecord" method="POST">
+<div class='addItem'>
+<form action="/admin/addrecord" class='addBookForm' method="POST">
+	<h2>Książka</h2>
 	<input type="hidden" name="action" value="ksiazka">
-	<input type="text" name="isbn" placeholder="ISBN"/>
+	<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+		<input class="mdl-textfield__input" type="text" id="p1" name="isbn"/>
+		<label class="mdl-textfield__label" for="p1">ISBN</label>
+	</div>
 	<?= form_error('isbn'); ?>
-	<input type="text" name="tytul_ksiazki" placeholder="Tytuł książki"/>
+	<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+		<input class="mdl-textfield__input" type="text" id="p2" name="tytul_ksiazki"/>
+		<label class="mdl-textfield__label" for="p2">Tytuł książki</label>
+	</div>
 	<?= form_error('tytul_ksiazki'); ?>
-	<select name="autor_ksiazki">
-	<?php foreach($pisarze as $pisarz): ?>
-		<option value="<?= $pisarz->id_autora ?>">
-			<?= $pisarz->imie_autora." ".$pisarz->nazwisko_autora ?>
-		</option>
-	<?php endforeach; ?>
-	</select>
+	<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+		<select class="mdl-textfield__input dropDownInput" id='p3' name="autor_ksiazki">
+		<?php foreach($pisarze as $pisarz): ?>
+			<option value="<?= $pisarz->id_autora ?>">
+				<?= $pisarz->imie_autora." ".$pisarz->nazwisko_autora ?>
+			</option>
+		<?php endforeach; ?>
+		</select>
+		<div class="mdl-button mdl-js-button mdl-button--icon addPopupBtn" onclick='showPopupAdd(1)'>
+			<i class="material-icons">add_box</i>
+		</div>
+		<label class="mdl-textfield__label" for="p3">Autor książki</label>
+	</div>
 	<?= form_error('autor_ksiazki'); ?>
-	<select name="epoka_ksiazki">
-	<?php foreach($epoki as $epoka): ?>
-		<option value="<?= $epoka->ID_epoki ?>">
-			<?= $epoka->nazwa_epoki ?>
-		</option>
-	<?php endforeach; ?>
-	</select>
+	<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+		<select class="mdl-textfield__input" id='p4' name="epoka_ksiazki">
+		<?php foreach($epoki as $epoka): ?>
+			<option value="<?= $epoka->ID_epoki ?>">
+				<?= $epoka->nazwa_epoki ?>
+			</option>
+		<?php endforeach; ?>
+		</select>
+		<label class="mdl-textfield__label" for="p4">Epoka książki</label>
+	</div>
 	<?= form_error('epoka_ksiazki'); ?>
-	<input type="date" name="data_wydania_ksiazki" placeholder="Data wydania"/>
+	<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+		<input class="mdl-textfield__input" type="date" id="p5" name="data_wydania_ksiazki"/>
+		<label class="mdl-textfield__label" for="p5">Data wydania</label>
+	</div>
 	<?= form_error('data_wydania_ksiazki'); ?>
-	<select name="wydawnictwo_ksiazki">
-	<?php foreach($wydawnictwa as $wydawnictwo): ?>
-		<option value="<?= $wydawnictwo->id_wydawnictwa ?>">
-			<?= $wydawnictwo->krotka_nazwa_wydawnictwa ?>
-		</option>
-	<?php endforeach; ?>
-	</select>
+	<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+		<select class="mdl-textfield__input dropDownInput" id='p6' name="wydawnictwo_ksiazki">
+		<?php foreach($wydawnictwa as $wydawnictwo): ?>
+			<option value="<?= $wydawnictwo->id_wydawnictwa ?>">
+				<?= $wydawnictwo->krotka_nazwa_wydawnictwa ?>
+			</option>
+		<?php endforeach; ?>
+		</select>
+		<div class="mdl-button mdl-js-button mdl-button--icon addPopupBtn" onclick='showPopupAdd(0)'>
+			<i class="material-icons">add_box</i>
+		</div>
+		<label class="mdl-textfield__label" for="p6">Wydawnictwo książki</label>
+	</div>
 	<?= form_error('wydawnictwo_ksiazki'); ?>
-	<select name="okladka">
-		<option value=1>Miękka</option>
-		<option value=2>Twarda</option>
-	</select>
-	<input type="number" name="liczba_egz" placeholder="Liczba egzemplarzy"/>
-	<input type="submit" value="Dodaj"/>
+	<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+		<select class="mdl-textfield__input" id='p7' name="okladka">
+			<option value=1>Miękka</option>
+			<option value=2>Twarda</option>
+		</select>
+		<label class="mdl-textfield__label" for="p7">Okładka</label>
+	</div>
+	<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+		<input class="mdl-textfield__input" type="number" id="p8" name="liczba_egz"/>
+		<label class="mdl-textfield__label" for="p8">Liczba egzemplarzy</label>
+	</div>
+	<input type="submit" value="Dodaj" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored"/>
 </form>
 
+<!------------------DODAWANIE AUTORA------------------------------->
+<div class='popupAlert' id='popupAutorKsiazki'>
+	<h2>Autor</h2>
+	<form action="/admin/addrecord" method="POST">
+		<input type="hidden" name="action" value="autor">
+		<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+			<input class="mdl-textfield__input" type="text" id="p1" name="imie_autora"/>
+			<label class="mdl-textfield__label" for="p1">Imię</label>
+		</div>
+		<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+			<input class="mdl-textfield__input" type="text" id="p2" name="nazwisko_autora"/>
+			<label class="mdl-textfield__label" for="p2">Nazwisko</label>
+		</div>
+		<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+			<input class="mdl-textfield__input" type="text" id="p3" name="pseudonim_autora"/>
+			<label class="mdl-textfield__label" for="p3">Pseudonim</label>
+		</div>
+		<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+			<input class="mdl-textfield__input" type="text" id="p4" name="data_urodzenia_autora"/>
+			<label class="mdl-textfield__label" for="p4">Data urodzenia autora</label>
+		</div>
+		<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+			<input class="mdl-textfield__input" type="text" id="p5" name="data_smierci_autora"/>
+			<label class="mdl-textfield__label" for="p5">Data śmierci autora</label>
+		</div>
+		<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+			<select class="mdl-textfield__input dropDownInput" id='p6' name="kraj_autora">
+			<?php foreach($kraje as $kraj): ?>
+				<option value="<?= $kraj->ID_kraju ?>">
+					<?= $kraj->nazwa_kraju ?>
+				</option>
+			<?php endforeach; ?>
+			</select>
+			<label class="mdl-textfield__label" for="p6">Kraj</label>
+		</div>
+		<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+			<select class="mdl-textfield__input dropDownInput" id='p7' name="typ_autora">
+				<option disabled selected>---</option>
+				<option value=1>Pisarz</option>
+				<option value=2>Reżyser</option>
+				<option value=3>Piosenkarz</option>
+			</select>
+			<label class="mdl-textfield__label" for="p7">Typ</label>
+		</div>
+		<input type="submit" value="Dodaj" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
+	</form>
+</div>
 <!------------------DODAWANIE WYDAWNICTWA---------------------------->
-<h2>Wydawnictwo</h2>
-<form action="/admin/addrecord" method="POST">
-	<input type="hidden" name="action" value="wydawnictwo">
-	<input type="text" name="nazwa_wydawnictwa" placeholder="Nazwa wydawnictwa"/>
-	<?= form_error('nazwa_wydawnictwa'); ?>
-	<input type="text" name="krotka_nazwa_wydawnictwa" placeholder="Krótka nazwa wydawnictwa"/>
-	<?= form_error('krotka_nazwa_wydawnictwa'); ?>
-	<input type="submit" value="Dodaj"/>
-</form>
-
+<div class='popupAlert' id='popupWydawnictwo'>
+	<h4>Wydawnictwo</h4>
+	<form action="/admin/addrecord" method="POST">
+		<input type="hidden" name="action" value="wydawnictwo">
+		<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+			<input class="mdl-textfield__input" type="text" id="p1" name="nazwa_wydawnictwa"/>
+			<label class="mdl-textfield__label" for="p1">Nazwa wydawnictwa</label>
+		</div>
+		<?= form_error('nazwa_wydawnictwa'); ?>
+		<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+			<input class="mdl-textfield__input" type="text" id="p2" name="krotka_nazwa_wydawnictwa"/>
+			<label class="mdl-textfield__label" for="p2">Krótka nazwa wydawnictwa</label>
+		</div>
+		<?= form_error('krotka_nazwa_wydawnictwa'); ?>
+		<input type="submit" value="Dodaj" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored"/>
+	</form>
+</div>
 <!-----------------DODAWANIE ALBUMU------------------------->
 <h2>Album</h2>
 <form action="/admin/addrecord" method="POST">
@@ -105,3 +158,5 @@
 	<input type="number" name="liczba_egzemplarzy_albumu" placeholder="Liczba egzemplarzy"/>
 	<input type="submit" value="Dodaj"/>
 </form>
+<div class="obfuscator"></div>
+</div>
