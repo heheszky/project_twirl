6131856519e6db1b2e86b3d5c46626dc607725cc
KsaneK <xaneqtv@gmail.com>
2015-10-25 16:04:28 +0100

    Poprawiony plik addrecord.php, zrobione ładne tagi

--- application/views/admin/addrecord.php
+++ application/views/admin/addrecord.php
@@ -3,30 +3,30 @@
 <form action="/admin/addrecord" method="POST">
 	<input type="hidden" name="action" value="autor">
 	<input type="text" name="imie_autora" placeholder="Imie"/>
-	<?php echo form_error('imie_autora'); ?>
+	<?= form_error('imie_autora'); ?>
 	<input type="text" name="nazwisko_autora" placeholder="Nazwisko"/>
-	<?php echo form_error('nazwisko_autora'); ?>
+	<?= form_error('nazwisko_autora'); ?>
 	<input type="text" name="pseudonim_autora" placeholder="Pseudonim"/>
-	<?php echo form_error('pseudonim_autora'); ?>
+	<?= form_error('pseudonim_autora'); ?>
 	<input type="date" name="data_urodzenia_autora"/>
-	<?php echo form_error('data_urodzenia_autora'); ?>
+	<?= form_error('data_urodzenia_autora'); ?>
 	<input type="date" name="data_smierci_autora"/>
-	<?php echo form_error('data_smierci_autora'); ?>
+	<?= form_error('data_smierci_autora'); ?>
 	<select name="kraj_autora">
-	<?php foreach($kraje as $kraj) { ?>
-		<option value="<?php echo $kraj->ID_kraju ?>">
-			<?php echo $kraj->nazwa_kraju ?>
+	<?php foreach($kraje as $kraj): ?>
+		<option value="<?= $kraj->ID_kraju ?>">
+			<?= $kraj->nazwa_kraju ?>
 		</option>
-	<?php } ?>
+	<?php endforeach; ?>
 	</select>
-	<?php echo form_error('kraj_autora'); ?>
+	<?= form_error('kraj_autora'); ?>
 	<select name="typ_autora">
 		<option disabled selected>---</option>
 		<option value=1>Pisarz</option>
 		<option value=2>Reżyser</option>
 		<option value=3>Piosenkarz</option>
 	</select>
-	<?php echo form_error('typ_autora'); ?>
+	<?= form_error('typ_autora'); ?>
 	<input type="submit" value="Dodaj">
 </form>
 
@@ -35,35 +35,35 @@
 <form action="/admin/addrecord" method="POST">
 	<input type="hidden" name="action" value="ksiazka">
 	<input type="text" name="isbn" placeholder="ISBN"/>
-	<?php echo form_error('isbn'); ?>
+	<?= form_error('isbn'); ?>
 	<input type="text" name="tytul_ksiazki" placeholder="Tytuł książki"/>
-	<?php echo form_error('tytul_ksiazki'); ?>
+	<?= form_error('tytul_ksiazki'); ?>
 	<select name="autor_ksiazki">
-	<?php foreach($pisarze as $pisarz) { ?>
-		<option value="<?php echo $pisarz->id_autora ?>">
-			<?php echo $pisarz->imie_autora." ".$pisarz->nazwisko_autora ?>
+	<?php foreach($pisarze as $pisarz): ?>
+		<option value="<?= $pisarz->id_autora ?>">
+			<?= $pisarz->imie_autora." ".$pisarz->nazwisko_autora ?>
 		</option>
-	<?php } ?>
+	<?php endforeach; ?>
 	</select>
-	<?php echo form_error('autor_ksiazki'); ?>
+	<?= form_error('autor_ksiazki'); ?>
 	<select name="epoka_ksiazki">
-	<?php foreach($epoki as $epoka) { ?>
-		<option value="<?php echo $epoka->ID_epoki ?>">
-			<?php echo $epoka->nazwa_epoki ?>
+	<?php foreach($epoki as $epoka): ?>
+		<option value="<?= $epoka->ID_epoki ?>">
+			<?= $epoka->nazwa_epoki ?>
 		</option>
-	<?php } ?>
+	<?php endforeach; ?>
 	</select>
-	<?php echo form_error('epoka_ksiazki'); ?>
+	<?= form_error('epoka_ksiazki'); ?>
 	<input type="date" name="data_wydania_ksiazki" placeholder="Data wydania"/>
-	<?php echo form_error('data_wydania_ksiazki'); ?>
+	<?= form_error('data_wydania_ksiazki'); ?>
 	<select name="wydawnictwo_ksiazki">
-	<?php foreach($wydawnictwa as $wydawnictwo) { ?>
-		<option value="<?php echo $wydawnictwo->id_wydawnictwa ?>">
-			<?php echo $wydawnictwo->krotka_nazwa_wydawnictwa ?>
+	<?php foreach($wydawnictwa as $wydawnictwo): ?>
+		<option value="<?= $wydawnictwo->id_wydawnictwa ?>">
+			<?= $wydawnictwo->krotka_nazwa_wydawnictwa ?>
 		</option>
-	<?php } ?>
+	<?php endforeach; ?>
 	</select>
-	<?php echo form_error('wydawnictwo_ksiazki'); ?>
+	<?= form_error('wydawnictwo_ksiazki'); ?>
 	<select name="okladka">
 		<option value=1>Miękka</option>
 		<option value=2>Twarda</option>
@@ -77,9 +77,9 @@
 <form action="/admin/addrecord" method="POST">
 	<input type="hidden" name="action" value="wydawnictwo">
 	<input type="text" name="nazwa_wydawnictwa" placeholder="Nazwa wydawnictwa"/>
-	<?php echo form_error('nazwa_wydawnictwa'); ?>
+	<?= form_error('nazwa_wydawnictwa'); ?>
 	<input type="text" name="krotka_nazwa_wydawnictwa" placeholder="Krótka nazwa wydawnictwa"/>
-	<?php echo form_error('krotka_nazwa_wydawnictwa'); ?>
+	<?= form_error('krotka_nazwa_wydawnictwa'); ?>
 	<input type="submit" value="Dodaj"/>
 </form>
 
@@ -88,18 +88,18 @@
 <form action="/admin/addrecord" method="POST">
 	<input type="hidden" name="action" value="album">
 	<input type="text" name="nazwa_albumu" placeholder="Nazwa albumu"/>
-	<?php echo form_error('nazwa_albumu'); ?>
+	<?= form_error('nazwa_albumu'); ?>
 	<input type="checkbox" name="kompilacja"/> Kompilacja 
 	<input type="date" name="data_wydania_albumu"/>
-	<?php echo form_error('data_wydania_albumu'); ?>
+	<?= form_error('data_wydania_albumu'); ?>
 	<input type="checkbox" name="soundtrack"/> Soundtrack
 	<input type="number" name="liczba_utworow" placeholder="Liczba utworów"/>
 	<select name="nosnik_fizyczny">
-	<?php foreach($nosniki as $nosnik) { ?>
-		<option value="<?php echo $nosnik->id_nosnika ?>">
-			<?php echo $nosnik->nazwa_nosnika ?>
+	<?php foreach($nosniki as $nosnik): ?>
+		<option value="<?= $nosnik->id_nosnika ?>">
+			<?= $nosnik->nazwa_nosnika ?>
 		</option>
-	<?php } ?>
+	<?php endforeach; ?>
 	</select>
 	<input type="number" name="liczba_nosnikow" placeholder="Liczba nośników"/>
 	<input type="number" name="liczba_egzemplarzy_albumu" placeholder="Liczba egzemplarzy"/>
